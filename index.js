cd16dc7513ab4add844cb6efda2be3452f49b2d9
screener-solutions <info@screener.solutions>
2025-07-22 19:48:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const { Configuration, OpenAIApi } = require("openai");
-require("dotenv\config");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
